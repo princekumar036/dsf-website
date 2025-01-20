cdc2334fda0a118a7f51d007c2ad30ddfd7954d3
princekumar036 <princekumar036@gmail.com>
2025-01-20 20:41:51 +0100

    moved indexjs to html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,41 +4,6 @@
     import Footer from './components/Footer.svelte';
     import Header from './components/Header.svelte';
 	let { children } = $props();
-
-	// NAV MENU TOGGLE
-	$(document).ready(function() {
-		$('#navtoggle').click(function() {
-			$('#navtoggle-open').toggle();
-			$('#navtoggle-close').toggle();
-			$('#navmenu').slideToggle();
-		})
-	})
-
-
-	// MOVEMENTS DESCRIPTION APPEARS ON HOVER ON LARGE SCREEN
-	if(window.screen.width >= 1024) {
-		$('#movements article').hover(function() {
-			$(this).find('#movement-description').slideToggle()
-		})
-	}
-
-
-	// MOVEMENTS SROLL ON CLICKING LEFT AND RIGHT BUTTON
-		// ON LARGE SCREEN
-	document.querySelector('#slide-right').addEventListener('click', function(e) {
-		document.querySelector('#movements').scrollBy({
-			top: 0,
-			left: 500,
-			behavior: 'smooth'
-			});
-	})
-	document.querySelector('#slide-left').addEventListener('click', function() {
-		document.querySelector('#movements').scrollBy({
-			top: 0,
-			left: -500,
-			behavior: 'smooth'
-			});
-	})
 </script>
 
 <svelte:head>
@@ -51,14 +16,14 @@
 	<meta property="og:url" content="{$page.url.href}" />
 	<meta property="og:title" content="Democratic Students' Federation" />
 	<meta property="og:description" content="DSF is an independent, leftwing students' organisation which seeks to strengthen the progressive and democratic students' movement across the country on the basis of independent, principled positions on the issues confronting the student community." />
-	<meta property="og:image" content="/star-black-lg.svg" />
+	<meta property="og:image" content="/files/star-black-lg.svg" />
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content="{$page.url.href}" />
 	<meta property="twitter:title" content="Democratic Students' Federation" />
 	<meta property="twitter:description" content="DSF is an independent, leftwing students' organisation which seeks to strengthen the progressive and democratic students' movement across the country on the basis of independent, principled positions on the issues confronting the student community." />
-	<meta property="twitter:image" content="/star-black-lg.svg" />
+	<meta property="twitter:image" content="/files/star-black-lg.svg" />
 </svelte:head>
 
 <Header />
