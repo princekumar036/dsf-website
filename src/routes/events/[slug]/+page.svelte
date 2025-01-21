d268493d0b4dd7deba334333f7374f6c52235ead
princekumar036 <princekumar036@gmail.com>
2025-01-21 09:50:49 +0100

    event sm meta title date fix

--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -16,14 +16,14 @@
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="{$page.url.href}" />
-	<meta property="og:title" content="{post.title} - {post.date}" />
+	<meta property="og:title" content="{post.title} ({post.date.toDateString()})" />
 	<meta property="og:description" content="{toHTML(post.content)}.substring(0, 150).replace(/<\/?[^>]+(>|$)/g, '')" />
 	<meta property="og:image" content={urlFor(post.eventImage).url()} />
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content="{$page.url.href}" />
-	<meta property="twitter:title" content="{post.title} - {post.date}" />
+	<meta property="twitter:title" content="{post.title} ({post.date.toDateString()})" />
 	<meta property="twitter:description" content="{toHTML(post.content)}.substring(0, 150).replace(/<\/?[^>]+(>|$)/g, '')" />
 	<meta property="twitter:image" content={urlFor(post.eventImage).url()} />
 </svelte:head>
